04a087c08ba8bc4355addeeb1ffae736865f8131
oizzoo <108180322+oizzoo@users.noreply.github.com>
2025-10-24 21:19:02 +0200

    feat: add rating validation and refactor toggle button in add-book

--- Public/js/hideForm.js
+++ Public/js/hideForm.js
@@ -1,13 +1,12 @@
 const bookForm = document.getElementById('book-form');
 const toggleFormButton = document.getElementById('toggle-form-button');
 
-//listen for click on the button
-toggleFormButton.addEventListener('click', () => {
-    bookForm.classList.toggle('hide');
-});
+
 
 // listen for click on the button to change text
 toggleFormButton.addEventListener('click', () => {
+    bookForm.classList.toggle('hide');
+    
     if (bookForm.classList.contains('hide')) {
         toggleFormButton.textContent = 'Add another book';
     } else {
